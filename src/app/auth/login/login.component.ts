d4c66b664f2c7b82aeab5ad2a746a651490f6bbf
Arlin Grijalba <arlin.grijalba@gmail.com>
2019-01-30 21:49:26 -0800

    Added login and signup components

---
+++ src/app/auth/login/login.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent {
+  isLoading = false;
+
+  onLogin(form: NgForm){
+
+  }
+}
---
+++ src/app/auth/signup/signup.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
+
+@Component({
+  templateUrl: './signup.component.html',
+  styleUrls: ['./signup.component.css']
+})
+export class SignupComponent {
+  isLoading = false;
+
+  onSignup(form: NgForm) {}
+}
