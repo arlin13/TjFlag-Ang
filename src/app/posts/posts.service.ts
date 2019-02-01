37168b3bd275bbf1fee889f5742650b3b9970d48
Arlin Grijalba <arlin.grijalba@gmail.com>
2019-01-31 21:38:19 -0800

    Authorization

--- src/app/posts/posts.service.ts
+++ src/app/posts/posts.service.ts
@@ -27,7 +27,8 @@ export class PostsService {
                 title: post.title,
                 content: post.content,
                 id: post._id,
-                imagePath: post.imagePath
+                imagePath: post.imagePath,
+                creator: post.creator
               };
             }),
             maxPosts: postData.maxPosts
@@ -53,6 +54,7 @@ export class PostsService {
       title: string;
       content: string;
       imagePath: string;
+      creator: string;
     }>('http://localhost:3000/api/posts/' + id);
   }
 
@@ -85,7 +87,8 @@ export class PostsService {
         id: id,
         title: title,
         content: content,
-        imagePath: image
+        imagePath: image,
+        creator: null
       };
     }
     this.http
